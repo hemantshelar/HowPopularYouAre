494779aa41e6c3d2df08190714992dab2e082a1d
hemantshelar <hemant.shelar@gmail.com>
2016-08-21 11:18:42 +1000

    Added Fraework,BingPopularityFinder and GooglePopularity finder projects.

---
+++ HowPopularYouAreUI/BingPopularityFinder/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingPopularityFinder
+{
+    public class GooglePopularityFinder : IPopularityFinder
+    {
+        public void SetSearchTerms(string[] terms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSearchTerms(List<string> terms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HowPopularYouAreUI/Framework/IPopularityFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public interface IPopularityFinder
+    {
+        void SetSearchTerms(string[] terms);
+        void SetSearchTerms(List<String> terms);
+        string GetResult();
+    }
+}
